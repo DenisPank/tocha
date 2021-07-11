381a58aa4051124badced97fbd78b80d997fddf7
DenisPank <den3000d.1267@gmail.com>
2021-07-11 22:27:42 +0300

    60%

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(".reviews__slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    centerMode: true,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
